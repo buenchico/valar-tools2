f7f84831cae5cddbfd71a2a77c52f0d653d2137b
buenchico <hammer_ortiz@hotmail.com>
2023-09-18 14:00:52 +0100

    refactor families loyalties

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -83,7 +83,6 @@ private
 
   def set_options
     @options = @tool.game_tools.find_by(game_id: active_game&.id)&.options
-    $options_families = @options
     if @options.nil?
       redirect_to settings_url, warning: 'Prepara una partida antes de usar la lista de familias'
     end
