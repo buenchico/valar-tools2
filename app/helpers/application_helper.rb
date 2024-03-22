e5638fc64d81e2a3e70e02e70444d8ba933b083d
buenchico <hammer_ortiz@hotmail.com>
2024-03-22 16:42:40 +0000

    Improve gender detection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,20 +35,18 @@ module ApplicationHelper
 
   def word_gender(word)
     if I18n.locale == :es
-      last_letter = word[-1].downcase
-
-      # Check if the last letter indicates the gender
-      if last_letter == 'a'
-        return 'feminine'
-      elsif last_letter == 'o'
-        return 'masculine'
+      # If the last letter doesn't indicate gender, check the YAML file
+      word_genders = I18n.t('word_genders', default: {}) # Load word genders from the YAML file
+      if word_genders.key?(word.downcase.to_sym)
+        return word_genders[word.downcase.to_sym]
       else
-        # If the last letter doesn't indicate gender, check the YAML file
-        word_genders = I18n.t('word_genders', default: {}) # Load word genders from the YAML file
-        if word_genders.key?(word)
-          return word_genders[word]
+        last_letter = word[-1].downcase
+
+        # Check if the last letter indicates the gender
+        if last_letter == 'a'
+          return 'feminine'
         else
-          return 'masculine' # Default to masculine when gender is not found
+          return 'masculine' # default to masculine
         end
       end
     else
