ffa847200700cc697fe6a68a4085a2cbf23a02e5
buenchico <hammer_ortiz@hotmail.com>
2024-06-18 15:27:03 +0100

    clocks updating on click

--- app/controllers/clocks_controller.rb
+++ app/controllers/clocks_controller.rb
@@ -1,5 +1,6 @@
 class ClocksController < ApplicationController
   before_action :set_tool
+  before_action :set_clock, only: [:show, :update]
 
   def index
     @clocks = Clock.all
@@ -7,6 +8,9 @@ class ClocksController < ApplicationController
 
   def new
     @clock = Clock.new
+  end
+
+  def show
   end
 
   def create
@@ -21,7 +25,24 @@ class ClocksController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @clock.update(clock_params)
+        flash.now[:success] = t('messages.success.update', thing: @clock.name.strip + " (id: " + @clock.id.to_s + ")", count: 1)
+        format.js
+      else
+        flash.now[:danger] = @clock.errors.to_hash
+        format.js { render 'layouts/error', locals: { thing: original_title + " (id: " + @clock.id.to_s + ")", method: 'update' } }
+      end
+    end
+  end
+
 private
+
+  def set_clock
+    @clock = Clock.find(params[:id])
+  end
+
   def clock_params
     params.require(:clock).permit(:name, :size, :status, :description, :family)
   end
