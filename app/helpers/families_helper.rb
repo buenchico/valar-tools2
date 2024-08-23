63c31b983cb6e7ed24721a44fcc4f9bef2719ac0
buenchico <hammer_ortiz@hotmail.com>
2024-08-23 19:07:40 +0100

    improved family relations

--- app/helpers/families_helper.rb
+++ app/helpers/families_helper.rb
@@ -1,2 +1,25 @@
 module FamiliesHelper
+  def snippet_for_members(family, keyword)
+    keyword_downcase = keyword.downcase
+    members_downcase = family.members.downcase
+    index = members_downcase.index(keyword_downcase)
+
+    return nil if index.nil?
+
+    # Find the start of the line containing the match
+    start_line_index = family.members.rindex("\n", index) || 0
+    start_index = [start_line_index, 0].max
+
+    # Find the end of the line after the match
+    end_line_index = family.members.index("\n", index)
+    end_index = end_line_index || family.members.length
+
+    # Extract the snippet
+    puts start_index
+    puts end_index
+    snippet = family.members[start_index...end_index]
+    puts snippet
+
+    return snippet
+  end
 end
