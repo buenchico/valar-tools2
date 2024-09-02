fdcb4aba8ba9bd9a30cbef730ac726edcfb08f12
buenchico <hammer_ortiz@hotmail.com>
2024-09-02 19:59:33 +0100

    scheme fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_27_154559) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_02_185148) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -208,17 +208,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_27_154559) do
     t.index ["faction_id"], name: "index_valar_missions_on_faction_id"
     t.index ["game_id"], name: "index_valar_missions_on_game_id"
     t.index ["user_id"], name: "index_valar_missions_on_user_id"
-  end
-
-  create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.string "section"
-    t.string "description"
-    t.integer "difficulty"
-    t.integer "speed"
-    t.text "factors", default: [], array: true
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "tools", force: :cascade do |t|
