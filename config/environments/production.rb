a7830ba67eb393c3ba1b97d5f04d87fce4e84f0d
buenchico <hammer_ortiz@hotmail.com>
2024-09-20 15:49:41 +0100

    activate lazy images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,4 +123,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Activate lazy images, imagaes outside the viewpoint will not be loaded
+  config.action_view.image_loading = :lazy
 end
