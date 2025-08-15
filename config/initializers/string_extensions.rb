077ae3042874602e40a9c8df1d75f31b76a6d516
beunchico <hammer_ortiz@hotmail.com>
2025-08-15 14:24:20 +0100

    pluralize all words

--- config/initializers/string_extensions.rb
+++ config/initializers/string_extensions.rb
@@ -1,5 +1,9 @@
 class String
+  IGNORE_WORDS = %w[the a of in on el la un uno una de].freeze
+
   def pluralize_all_words(count = nil)
-    split.map { |word| word.pluralize(count) }.join(' ')
+    split.map do |word|
+      IGNORE_WORDS.include?(word.downcase) ? word : word.pluralize(count)
+    end.join(' ')
   end
 end
