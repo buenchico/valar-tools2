c4d9c8cd5b11102a9c64a42dd16e3f84e4381a4a
beunchico <hammer_ortiz@hotmail.com>
2025-09-01 16:18:41 +0100

    migration for new units

---
+++ db/migrate/20250901151200_armies_refactor.rb
@@ -0,0 +1,34 @@
+class ArmiesRefactor < ActiveRecord::Migration[8.0]
+  def change
+    drop_table :armies
+    drop_table :units
+
+    create_table :armies do |t|
+      t.string :name, null: false
+      t.string :position
+      t.text :notes
+      t.integer "xp", default: 100
+      t.integer "morale", default: 100
+      t.text "logs", default: [], array: true
+
+      t.timestamps
+    end
+
+    create_table :units do |t|
+      t.string :name, null: false
+      t.string :unit_type
+      t.integer :count
+      t.integer :count_start
+      t.integer :count_death
+      t.integer :strength
+      t.integer :strength_indirect
+      t.integer :hp
+
+      t.references :army, foreign_key: true
+      t.references :family, foreign_key: true
+      t.references :location, foreign_key: true  # optional
+
+      t.timestamps
+    end
+  end
+end
