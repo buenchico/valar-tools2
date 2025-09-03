2ec70275bb75fb2904b73d38a95f5bd37682ad96
beunchico <hammer_ortiz@hotmail.com>
2025-09-03 20:01:22 +0100

    get army function

--- app/controllers/armies_controller.rb
+++ app/controllers/armies_controller.rb
@@ -14,12 +14,39 @@ def index
       @units = @faction.units.where(visible: true, army_id: nil).order(:id)
     else
       @armies = nil
-      @units = Unit.all
+      @units = nil
     end
   else
     @faction = @current_user.faction
     @armies = @faction.armies.where(visible: true).order(:id)
     @units = @faction.units.where(visible: true, army_id: nil).order(:id)
+  end
+end
+
+def get_armies
+  origin = URI(request.referer).path.split('/')[1]
+
+  @active_factions = JSON.parse(params[:active_factions])
+  @active_visibility = JSON.parse(params[:active_visibility])
+
+  @armies_all = Army.where(visible: true)
+
+  master = Faction.find_by(name: 'master')
+
+  if @active_factions.count == 1
+    @faction = Faction.find_by(id: @active_factions)
+  end
+
+  if @active_factions.include?(master.id.to_s)
+    @armies = Army.all.where(visible: @active_visibility)
+    @units = Unit.all.where(visible: @active_visibility)
+  else
+    @armies = Army.joins(:factions).where(visible: @active_visibility).where(factions: { id: @active_factions })
+    @units = Unit.joins(:factions).where(visible: @active_visibility).where(factions: { id: @active_factions })
+  end
+
+  respond_to do |format|
+    format.js
   end
 end
 
