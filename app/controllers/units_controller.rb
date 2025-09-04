5214961a947bb8c239fd744d5267fe4a93ea3a76
beunchico <hammer_ortiz@hotmail.com>
2025-09-04 17:05:31 +0100

    join into army WIP

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -3,7 +3,7 @@ class UnitsController < ApplicationController
   before_action :set_factions, only: [:new, :edit, :new_multiple, :edit_multiple]
   before_action :set_regions, only: [:new, :edit, :new_multiple]
   before_action :set_unit, only: [:edit, :update, :destroy, :delete]
-  before_action :check_master, only: [:new, :edit, :delete, :edit_multiple, :create, :destroy, :delete, :update_multiple, :destroy_multiple]
+  before_action :check_master, only: [:new, :edit, :delete, :edit_multiple, :create, :destroy, :delete, :update_multiple, :destroy_multiple, :create_army]
   before_action :check_owner, only: [:edit, :edit_notes, :update]
 
   def new
@@ -20,6 +20,8 @@ class UnitsController < ApplicationController
   def edit_multiple
     unit_ids = params[:unit_ids].split(',')
     @units = Unit.where(id: unit_ids).order(:name)
+    @army_type = @units.map(&:army_type).uniq
+    @armies = Army.includes(:units).select { |army| army.army_type == @army_type[0] }
     @template = 'form_' + params[:button] + '_multiple'
   end
 
--- app/models/army.rb
+++ app/models/army.rb
@@ -1,7 +1,15 @@
 class Army < ApplicationRecord
   include PgSearch::Model
-  multisearchable against: [:name, :group, :position, :notes, :search]
+  multisearchable against: [:name, :position, :notes, :search]
 
   has_many :units, dependent: :destroy
   has_and_belongs_to_many :factions
+
+  def search
+   nil
+  end
+
+  def army_type
+    units.first.army_type
+  end
 end
