55b9c430b1d68c70b45dc988f645b431ccaaabc4
beunchico <hammer_ortiz@hotmail.com>
2025-09-25 12:58:15 +0100

    improve faction stats

--- app/helpers/armies_helper.rb
+++ app/helpers/armies_helper.rb
@@ -9,9 +9,9 @@ module ArmiesHelper
                  count: sorted_units.sum { |u| u.count.to_i },
                  count_death: sorted_units.sum { |u| u.count_death.to_i },
                  men: sorted_units.sum { |u| u.men.to_i },
-                 strength: sorted_units.sum { |u| u.strength.to_i },
+                 strength: sorted_units.sum { |u| u.strength.to_f },
                  strength_indirect: sorted_units.sum { |u| u.strength_indirect.to_f },
-                 hp: sorted_units.sum { |u| u.hp.to_f },
+                 hp: sorted_units.sum { |u| u.hp.to_i },
                  icon: sorted_units.first.icon,
                  colour: sorted_units.first.colour,
                  speed: sorted_units.map(&:speed).max,
@@ -19,6 +19,6 @@ module ArmiesHelper
                }
              end
          end
-         .to_h
+         .sort.to_h  
   end
 end
